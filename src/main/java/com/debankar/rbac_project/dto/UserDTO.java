11890c5508ff7543cd92094ead7abe9316ebda4c
deedeecee <dcdebankar@gmail.com>
2024-12-03 23:04:38 +0530

    Fixed logical error in UserDTO

--- src/main/java/com/debankar/rbac_project/dto/UserDTO.java
+++ src/main/java/com/debankar/rbac_project/dto/UserDTO.java
@@ -16,8 +16,8 @@ import java.util.Set;
 @AllArgsConstructor
 public class UserDTO {
     private Long id;            // The unique identifier of the user.
-    private String email;       // The email address of the user.
     private String username;    // The username of the user.
+    private String email;       // The email address of the user.
 
     // A set of roles assigned to the user. This provides information about the user's permissions and access levels.
     private Set<Role> roles;
